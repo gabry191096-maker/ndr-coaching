bb5ff729d456a0c36c287436a683ae66ba3dd874
gabry191096-maker <gabry191096@gmail.com>
2025-11-12 23:58:49 +0000

    Fix HTML structure - add missing closing anchor tag

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -37,8 +37,9 @@ export default function ContactPage() {
                                                                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                                                                               </svg>
                     <a href="tel:+447444035852" className="text-xl text-white hover:text-primary transition-colors">                                                                                                +44 7123 456789
-                      +44 7444 035852                                                                      </div>
-                                                </div>
+                      +44 7444 035852
+                    </a>
+                                                                    </div></div>
             </div>
           </div>
         </div>
